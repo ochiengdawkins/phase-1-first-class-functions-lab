cbcc5b0598841de3b119131d89210b0bf9aa184c
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 11:36:47 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array = []) => {
+    return array.slice(0,2);
+};
+const returnLastTwoDrivers = (array = []) => {
+    return array.slice(array.length - 2, array.length);
+};
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(argument){
+    return pizza => argument * pizza; 
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, cb){
+    return cb(drivers);
+} 
